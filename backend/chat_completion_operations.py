ea0d999f9b9904223f05fc99e19ab750213aecca
Siddarth-Sajeev22 <siddarthsajeev2002@gmail.com>
2025-01-06 01:29:35 +0530

    fixing some things

--- backend/chat_completion_operations.py
+++ backend/chat_completion_operations.py
@@ -10,6 +10,10 @@ class ChatCompletion :
             api_key = os.getenv("GROQ_API_KEY")
         )
     def generate_rag_answer(self, *args, **kwargs):
+
+        system_message = """
+            You are an AI assistant that will help in making informed decisions regarding whether or not a given IPO is good fundamentally to buy in the stock market. You will be provided context regarding the IPO from a DRHP document and you will provide information to the user on the basis of the context provided to you only. Do not try to provide any information that is not available from the context
+        """
         completion = self.client.chat.completions.create(
             model="llama-3.1-70b-versatile",
             messages=[
--- backend/db_service.py
+++ backend/db_service.py
@@ -1,16 +1,19 @@
+import os
+from dotenv import load_dotenv
 import pymongo
 import re
 from generate_embedding import EmbeddingOperation
 
+load_dotenv()
 class DatabaseService : 
     def __init__(self, db_name, collection_name) : 
-        self.client = pymongo.MongoClient('mongo')
+        self.client = pymongo.MongoClient(os.getenv("DB_CONNECTION_STRING"))
         self.db = db_name
         self.collection = collection_name
 
     def create_embeddings_for_data(self): 
         eb = EmbeddingOperation()
-        database = self.db
+        database = self.client[self.db]
         collection = database[self.collection]
         docs = collection.find()
         for doc in docs : 
@@ -22,19 +25,19 @@ class DatabaseService :
                 )
     
     def insert_drhp_data_to_db(self): 
-        database = self.database
+        database = self.client[self.db]
         collection = database[self.collection]
-        pattern = r'(?m)^(\d+)\s' 
-        with open('drhp.md', 'r', encoding='utf-8') as file : 
+        pattern = r'(?m)^\s*\d+\s' 
+        with open('backend/drhp.md', 'r', encoding='utf-8') as file : 
             text = file.read()
         matches = list(re.finditer(pattern, text))
         for i in range(len(matches)) : 
             start = matches[i].end()
-            end = matches[i+1].end if i + 1 < len(matches) else len(text)
+            end = matches[i+1].start() if i + 1 < len(matches) else len(text)
 
-            db_text = text[start:end]
+            db_text = text[start:end].strip()
             collection.insert_one({
-                {"text": db_text}
+                "text": db_text
             })
 
 
